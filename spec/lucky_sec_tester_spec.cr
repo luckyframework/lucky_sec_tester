a61e9599cb13740c560dca71d6159fcf3ff1771b
Jeremy Woertink <jeremywoertink@gmail.com>
2022-03-27 10:46:41 -0700

    Allow overriding the headers (#8)
    * Allow overriding the headers. Also making the client lazy evaluated for better testing. Along with some (a) tests. Fixes #6
    
    * newline cinema
    
    * Need to delegate to the getter so the SecTester will be instantiated on call

--- spec/lucky_sec_tester_spec.cr
+++ spec/lucky_sec_tester_spec.cr
@@ -1,5 +1,30 @@
 require "./spec_helper"
 
+class IndexAction < Lucky::Action
+  method "get"
+  path "/test"
+end
+
+class CreateAction < Lucky::Action
+  method "post"
+  path "/make"
+end
+
 describe LuckySecTester do
   # https://github.com/NeuraLegion/sec_tester
+
+  it "overrides the default target header" do
+    scanner = LuckySecTester.new
+    target = scanner.build_target(IndexAction)
+    target.headers["Content-Type"].should eq("text/html")
+
+    target = scanner.build_target(CreateAction)
+    target.headers["Content-Type"].should eq("application/x-www-form-urlencoded")
+
+    target = scanner.build_target(IndexAction, headers: HTTP::Headers{"Content-Type" => "application/json"})
+    target.headers["Content-Type"].should eq("application/json")
+
+    target = scanner.build_target(CreateAction, headers: HTTP::Headers{"Content-Type" => "application/json"})
+    target.headers["Content-Type"].should eq("application/json")
+  end
 end
